51cda2c7e093db4d5867c0ef864343fa5b70a233
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-16 17:53:40 -0300

    Carpeta ViewModels
    Clase LoginVModel, Clase RegisterVModel

---
+++ Login_Identity/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login_Identity.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El {0} es requerido")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Display(Name = "Recuerdame")]
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ Login_Identity/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login_Identity.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "El password no coincide")]
+        public string? ConfirmPassword { get; set; }
+        public string? Address { get; set; }
+    }
+}
