1bad1e5064ce7aef026dd1e23b3cc1eeae915f79
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-17 13:27:53 -0300

    Metodo Post LoginAsync
    Add Authorize en HomeController

--- Login_Identity/Controllers/AccountController.cs
+++ Login_Identity/Controllers/AccountController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Login_Identity.Models;
+using Login_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Login_Identity.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //nos logueamos
+                var result = await _signInManager.PasswordSignInAsync(model.UserName!, model.Password!, model.RememberMe!, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                //En caso de error
+                ModelState.AddModelError("", "login inválido");
+                return View(model);
+            }
+
+            return View(model);
         }
 
         public IActionResult Register()
--- Login_Identity/Controllers/HomeController.cs
+++ Login_Identity/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Login_Identity.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Login_Identity.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
