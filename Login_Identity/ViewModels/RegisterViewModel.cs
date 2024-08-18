ccf72e70c55f34d76d915dc3f2839fead4ef2b19
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-18 12:27:11 -0300

    Vista Register
    Add DataAnnotations

--- Login_Identity/ViewModels/RegisterViewModel.cs
+++ Login_Identity/ViewModels/RegisterViewModel.cs
@@ -4,19 +4,24 @@ namespace Login_Identity.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "El {0} es requerido")]
+        [Display(Name = "Nombre")]
         public string? Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El {0} es requerido")]
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El {0} es requerido")]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
 
         [Compare("Password", ErrorMessage = "El password no coincide")]
+        [Display(Name = "Confirmar Password")]
         public string? ConfirmPassword { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Dirección")]
         public string? Address { get; set; }
     }
 }
